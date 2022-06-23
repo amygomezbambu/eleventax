398eac1d785f3eef5a38aacee8425bbafa8b5572
Luis Carrasco <luis@bambucode.com>
2022-06-23 12:20:21 -0600

    feat - Se genera la UI dependiendo del tamaño de pantalla usando LayoutBuilder.

--- lib/modules/sales/ui/sales_page.dart
+++ lib/modules/sales/ui/sales_page.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../../common/ui/ui_consts.dart' as ui;
 import '../../common/ui/primary_button.dart';
-import 'package:flutter_tailwindcss_defaults/colors.dart';
 import 'sale_items_actions.dart';
 import 'ui_sale_item.dart';
 import 'sale_items_list.dart';
 
 class SalesPage extends StatefulWidget {
-  const SalesPage({Key? key, required this.title}) : super(key: key);
-
   final String title;
+
+  const SalesPage({Key? key, required this.title}) : super(key: key);
 
   @override
   State<SalesPage> createState() => _SalesPageState();
@@ -19,33 +18,57 @@ class SalesPage extends StatefulWidget {
 class _SalesPageState extends State<SalesPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-      children: [
-        AppBar(
-          toolbarHeight: 50,
-          title: Text(
-            MediaQuery.of(context).size.width > 800
-                ? 'eleventa desktop'
-                : 'eleventa móvil',
-            style: GoogleFonts.openSans(),
-          ),
-          backgroundColor: ui.backgroundColor,
-          foregroundColor: Colors.white,
-          elevation: 0,
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.add_shopping_cart_rounded),
-              color: Color.fromARGB(255, 179, 224, 255),
-              tooltip: 'Agregar venta rápida',
-              onPressed: () {},
-            )
+    return LayoutBuilder(
+        builder: (BuildContext ctx, BoxConstraints constraints) {
+      // Desktop / Tablet view
+      if (constraints.maxWidth >= 800) {
+        return Scaffold(
+            body: Row(
+          children: [
+            Container(
+              color: ui.backgroundColor,
+              width: 60,
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  SaleItemsContainer(),
+                ],
+              ),
+            ),
           ],
-          leading: const Icon(Icons.menu),
-        ),
-        SaleItemsContainer()
-      ],
-    ));
+        ));
+      } else {
+        // Mobile
+        return Scaffold(
+            body: Column(
+          children: [
+            AppBar(
+              toolbarHeight: 50,
+              title: Text(
+                MediaQuery.of(context).size.width > 800
+                    ? 'eleventa desktop'
+                    : 'eleventa móvil',
+                style: GoogleFonts.openSans(),
+              ),
+              backgroundColor: ui.backgroundColor,
+              foregroundColor: Colors.white,
+              elevation: 0,
+              actions: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.add_shopping_cart_rounded),
+                  color: const Color.fromARGB(255, 179, 224, 255),
+                  tooltip: 'Agregar venta rápida',
+                  onPressed: () {},
+                )
+              ],
+              leading: const Icon(Icons.menu),
+            ),
+            SaleItemsContainer()
+          ],
+        ));
+      }
+    });
   }
 }
 
@@ -61,14 +84,14 @@ class SaleItemsContainer extends StatefulWidget {
 class _SaleItemsContainerState extends State<SaleItemsContainer> {
   @override
   Widget build(BuildContext context) {
+    // TBD: Sacar el breakpoint para desktop de estandares o un paquete auxilair
     return MediaQuery.of(context).size.width > 800
         ? Expanded(
             child: Row(
               children: [
-                SaleItemsList(),
+                const SaleItemsList(),
                 Container(
                   width: 400,
-                  //height: double.maxFinite,
                   padding: const EdgeInsets.fromLTRB(1, 5, 10, 5),
                   child: Column(
                     children: [
@@ -83,8 +106,9 @@ class _SaleItemsContainerState extends State<SaleItemsContainer> {
                       Container(
                         margin: const EdgeInsets.fromLTRB(3, 10, 5, 10),
                         height: 60,
-                        child: const PrimaryButton(
-                            'Cobrar 12,445.50', Icons.attach_money_outlined),
+                        child: PrimaryButton(
+                            'Cobrar ${UiCart.total.toString()}',
+                            Icons.attach_money_outlined),
                       )
                     ],
                   ),
@@ -95,50 +119,15 @@ class _SaleItemsContainerState extends State<SaleItemsContainer> {
         : Expanded(
             child: Column(
               children: [
-                SaleItemsList(),
+                const SaleItemsList(),
                 Container(
                   margin: const EdgeInsets.all(10),
                   height: 60,
-                  child: const PrimaryButton(
-                      'Cobrar \$12,445.50', Icons.attach_money_outlined),
+                  child: PrimaryButton('Cobrar ${UiCart.total.toString()}',
+                      Icons.attach_money_outlined),
                 )
               ],
             ),
           );
   }
 }
-
-// class TopNavigation extends StatelessWidget {
-//   final String title;
-
-//   const TopNavigation({
-//     required this.title,
-//     Key? key,
-//   }) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       children: [
-//         Expanded(
-//             child: Container(
-//           color: Colors.blue,
-//           height: 60,
-//           child: Row(
-//             children: [
-//               const Padding(
-//                 padding: EdgeInsets.all(2.0),
-//                 child: FlutterLogo(),
-//               ),
-//               Padding(
-//                 padding: const EdgeInsets.all(8.0),
-//                 child: Text(title,
-//                     style: const TextStyle(fontSize: 20, color: Colors.white)),
-//               ),
-//             ],
-//           ),
-//         )),
-//       ],
-//     );
-//   }
-// }
