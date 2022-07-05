abe7980666a20139ff2edae8ca204b67d6b946cb
Manuel Alejandro Gamboa Jimenez <58448452+manuel-gamboa@users.noreply.github.com>
2022-07-05 10:43:38 -0600

    Mob Session 5 Julio

--- lib/modules/common/infra/sqlite_adapter.dart
+++ lib/modules/common/infra/sqlite_adapter.dart
@@ -34,7 +34,7 @@ class SQLiteAdapter implements IDatabaseAdapter {
     } else {
       dbPath = (await getApplicationDocumentsDirectory()).path;
       dbPath = join(dbPath, 'eleventa.db');
-      print('Conectado a BD: $dbPath');
+      print('Conectado a BD: $dbPath'); //prueba
     }
 
     if (defaultTargetPlatform == TargetPlatform.windows) {
