2ba27938961638661749909c8a3e73d5264fec06
Raul Mendez <raul.mendez@bambucode.com>
2022-10-27 14:56:18 -0600

    actualizar task y dlls

---
+++ .githooks/pre-commit.ps1
@@ -0,0 +1,42 @@
+﻿# Buscamos ubicacion de comando flutter 
+
+#Set-Variable -Name "USER" -Value ($env:UserName)
+#Write-Host $USER
+
+
+#Revisamos si existe la variable de entorno FLUTTER_ROOT
+if (!(Test-Path 'env:FLUTTER_ROOT')) { 
+  Write-Host -ForegroundColor Red "Define la variable de entorno FLUTTER_ROOT"
+  exit 1
+}
+
+if (! $?)
+{
+    Write-Host -ForegroundColor Red "Error al buscar la variable de entorno FLUTTER_ROOT"
+    exit 1
+}  
+
+Write-Host -ForegroundColor Green "=== Ejecutando Flutter Analyzer! ==="
+
+flutter analyze 
+
+if (! $?)
+{
+    Write-Host -ForegroundColor Red "Error al ejecutar Flutter Analyzer"
+    exit 1
+}
+
+
+Write-Host -ForegroundColor Green "✔ Flutter Analyze ejecutado correctamente."
+
+Write-Host -ForegroundColor Green "=== Ejecutando pruebas de unidad ==="
+
+flutter test --test-randomize-ordering-seed=random -rexpanded
+
+if (! $?)
+{
+    Write-Host -ForegroundColor Red "Error en prueba de unidad"
+    exit 1
+}
+
+Write-Host -ForegroundColor Green "✔ Flutter test ejecutado correctamencte"
---
+++ tools/borrar_bd_local.ps1
@@ -0,0 +1,9 @@
+﻿$mydocuments = [environment]::getfolderpath("mydocuments")
+
+if (Test-Path ("$mydocuments\eleventa.db")) { 
+    remove-Item -Path "$mydocuments\eleventa.db" -Force   
+    Write-Host "Base de datos eliminada"
+}
+else {
+    Write-Host "Base de datos no existe"
+}
