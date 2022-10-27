2cac657389024f49e64884c8e9aa8154f62576bd
Raul Mendez <raul.mendez@bambucode.com>
2022-10-27 15:35:59 -0600

    test commit symlinks

---
+++ tools/setup_githooks.ps1
@@ -0,0 +1,13 @@
+﻿Set-Variable -Name "GIT_DIR" -Value (git rev-parse --git-dir)
+Set-Variable -Name "RAIZ" -Value "C:\Code\eleventax"
+
+Write-Host $RAIZ
+Write-Host $GIT_DIR
+Write-Host "Instalando hooks..."
+
+# this command creates symlink to our pre-commit script
+Write-Host "Creando symbolic link en directorio $GIT_DIR"
+New-Item -ItemType SymbolicLink -Path "$RAIZ\$GIT_DIR\hooks\pre-commit" -Target "$RAIZ\.githooks\pre-commit.ps1"
+ICACLS "$RAIZ\.githooks\pre-commit.ps1" /grant:r "everyone:(F)" /C
+
+Write-Host "Listo!" 
